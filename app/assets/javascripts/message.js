6ba82f54e7afee0bbb9148ecf2ba23805d1ad87e
fkoyo <fu.ko.0428@gmail.com>
2020-02-24 15:08:44 +0900

    create_appendeHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = `<div class="message">
+                    <div class="message__title">
+                      <div class="message__title--name">
+                      ${message.user.name}
+                      </div>
+                      <div class="message__title--date">
+                      ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__lower">
+                      <div class="message__lower--text">
+                      ${message.content}
+                      </div>
+                      <img class="message__image" src=${message.image} >
+                    </div>
+                  </div>`//メッセージに画像が含まれる場合のHTMLを作る
+    } else {
+      var html = `<div class="message">
+                    <div class="message__title">
+                      <div class="message__title--name">
+                      ${message.user.name}
+                      </div>
+                      <div class="message__title--date">
+                      ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__lower">
+                      <div class="message__lower--text">
+                      ${message.content}
+                      </div>
+                    </div>
+                  </div>`//メッセージに画像が含まれない場合のHTMLを作る
+    }
+    return html
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +51,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".maincontents").append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
+      // $('.messagesubmit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
 });
